5a4a8f5339bdcbb43d3e703e44cdf2583bb6a2f7
ECKO419 <ecko419117@gmail.com>
2026-08-01 13:08:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+boton.addEventListener("click",()=>{
+
+alert("¡Eres lo más bonito que me ha pasado ❤️!");
+
+mensaje.innerHTML=`
+
+Mi hermosa <b>Danita</b> ❤️<br><br>
+
+Hoy quiero aprovechar este día para decirte lo importante que eres para mí.<br><br>
+
+Gracias, mi amor, por estar conmigo aun cuando no he tenido nada. Nunca olvidaré que has permanecido a mi lado en los momentos difíciles y que has compartido conmigo tanto las alegrías como los desafíos.<br><br>
+
+Te mereces todo lo bonito de este mundo, y trabajaré cada día para poder dártelo.<br><br>
+
+Gracias por tu paciencia, por tu cariño y por creer en mí.<br><br>
+
+❤️ Feliz Día de la Novia ❤️<br><br>
+
+Te amo muchísimo y espero seguir celebrando muchos momentos como este a tu lado. 💕
+
+`;
+
+boton.style.display="none";
+
+crearCorazones();
+
+});
