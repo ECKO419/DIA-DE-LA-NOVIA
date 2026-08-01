c35710a5bab1ad5b8976f86a81ca85c5bc554928
ECKO419 <ecko419117@gmail.com>
2026-08-01 13:02:48 -0500

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-const boton = document.getElementById("boton");
-
-const mensaje = document.getElementById("mensaje");
-
-
 boton.addEventListener("click",()=>{
 
+alert("¡Eres lo más bonito que me ha pasado ❤️!");
 
 mensaje.innerHTML=`
 
@@ -24,50 +20,8 @@ Te amo muchísimo y espero seguir celebrando muchos momentos como este a tu lado
 
 `;
 
-
 boton.style.display="none";
-
 
 crearCorazones();
 
-
 });
-
-
-
-function crearCorazones(){
-
-
-for(let i=0;i<30;i++){
-
-
-let corazon=document.createElement("div");
-
-corazon.className="corazon";
-
-corazon.innerHTML="❤️";
-
-
-corazon.style.left=Math.random()*100+"vw";
-
-corazon.style.bottom="-50px";
-
-corazon.style.fontSize=(20+Math.random()*35)+"px";
-
-corazon.style.animationDuration=(3+Math.random()*5)+"s";
-
-
-document.body.appendChild(corazon);
-
-
-setTimeout(()=>{
-
-corazon.remove();
-
-},7000);
-
-
-}
-
-
-}
